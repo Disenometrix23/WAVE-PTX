f24a4ca6f110231c76a82d33cd278c14b0b6880f
DWbriana <125290562+DWbriana@users.noreply.github.com>
2023-02-17 19:26:51 -0500

    video player modal

---
+++ assets/js/video-modal.js
@@ -0,0 +1,20 @@
+// Function to reveal lightbox and adding YouTube autoplay
+function revealVideo(div,video_id) {
+	var video = document.getElementById(video_id).src;
+	document.getElementById(video_id).src = video; // adding autoplay to the URL
+	document.getElementById(div).style.display = 'block';
+}
+
+// Hiding the lightbox and removing YouTube autoplay
+function hideVideo(div,video_id) {
+	var video = document.getElementById(video_id).src;
+	var cleaned = video.replace('&autoplay=1',''); // removing autoplay form url
+	document.getElementById(video_id).src = cleaned;
+	document.getElementById(div).style.display = 'none';
+}
+
+$(document).keydown(function(event) { 
+  if (event.keyCode == 27) { 
+    hideVideo('video','youtube');
+  }
+});
